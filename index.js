c645c56ed0400022a90a535fa009fbce0f6f50b5
abijuru_seth <abiheloaf@gmail.com>
2021-03-22 20:33:29 +0200

    updated index

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Rwanda provinces!')
+    res.send('Welcome to the Rwanda provinces!');
 })
 
 app.use('/api/province/', provinceRouter);
