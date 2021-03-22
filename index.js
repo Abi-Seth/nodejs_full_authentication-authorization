39b61ec50d841dea10bdfc95172af14486038881
abijuru_seth <abiheloaf@gmail.com>
2021-03-22 20:33:10 +0200

    updated routers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const config = require('config');
 const bodyParser = require('body-parser');
-const provinceRouter = require('./routers/provinces.router')
+const provinceRouter = require('./routers/provinces.router');
 
 require('./models/mongodb');
 
