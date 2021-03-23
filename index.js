99e48d3f5987ca13ed437422838f684f9f218133
abijuru_seth <abiheloaf@gmail.com>
2021-03-23 13:11:00 +0200

    updated index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('./models/mongodb');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
