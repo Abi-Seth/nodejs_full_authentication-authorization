7d4b06b5eab9f0e63289be61436934ec655dd4d8
abijuru_seth <abiheloaf@gmail.com>
2021-03-24 21:22:07 +0200

    included bcrypt and saved a user with a hashed pass

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -24,6 +24,18 @@ exports.addUser = async (req, res) => {
             }).status(403)
         }
 
+        let newUser = new User(_.pick(user, ['Username', 'email', 'password', 'isAdmin']));
+        const hashedPassword = await hashPassword(newUser.password);
+        newUser.password = hashedPassword;
+
+        await newUser.save()
+        res.send({
+            success: true,
+            status: 201,
+            message: 'User account created successfully',
+            userDetails: _.pick(newUser, ['_id', 'Username', 'email', 'isAdmin'])
+        }).status(201)
+
     } catch(err) {
         res.send({
             success: false,
